0113c2d0f261b9310938ff9022f5aed91926bdfc
Lin Dong <ldong@gatech.edu>
2016-05-14 15:37:45 -0700

    Done Intermediate Meteor Tutorial #22 - Updating With A QuickForm

--- web/client/recipes/Recipe.js
+++ web/client/recipes/Recipe.js
@@ -7,3 +7,10 @@ Template.Recipe.events({
     Meteor.call('toggleMenuItem', this._id, this.inMenu);
   }
 });
+
+
+Template.Recipe.helpers({
+  updateRecipeId: function() {
+    return this._id;
+  }
+});
